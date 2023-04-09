016a4dbae67dd6b751235f1db0425b8bd8b92841
fuku0717 <zhenchengs314@gmail.com>
2023-04-09 22:30:17 +0900

    ログイン、新規登録ページへの遷移

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,10 @@
 class ApplicationController < ActionController::Base
-  before_action :basic_auth
-
+  before_action :basic_auth, if: :devise_controller?
 
   private
+  def basic_auth
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
+  end
 
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
