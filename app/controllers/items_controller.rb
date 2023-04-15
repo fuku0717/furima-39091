99b72e26e8aeabedd5d0b4c49839cc89b9590543
fuku0717 <zhenchengs314@gmail.com>
2023-04-15 18:54:56 +0900

    価格の保存条件

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -21,7 +21,7 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:item_name, :explanation, :category, :condition, :shipping_charge, :prefecture, :days_to_ship, :price, :user, :image).merge(user_id: current_user.id)
+    params.require(:item).permit(:item_name, :explanation, :category_id, :condition_id, :shipping_charge_id, :prefecture_id, :days_to_ship_id, :price, :user, :image).merge(user_id: current_user.id)
   end
 
 
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,12 +1,9 @@
 class Item < ApplicationRecord
   validates :item_name, presence: true
   validates :explanation, presence: true
-  validates :category_id, presence: true
-  validates :condition_id, presence: true
-  validates :shipping_charge_id, presence: true
-  validates :prefecture_id, presence: true
-  validates :days_to_ship_id, presence: true
   validates :price, presence: true
+  validates :price, numericality: { with: /\A[0-9]+\z/, message: 'Half-width number' }
+  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: 'Out of setting range' }
   validates :user, presence: true
 
   has_one_attached :image
@@ -14,20 +11,19 @@ class Item < ApplicationRecord
 
 
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :prefecture_id 
-  validates :prefecture_id, presence: true, numericality: { other_than: 1 , message: "can't be blank" }
+  belongs_to :category
+  validates :category_id, numericality: { other_than: 1 , message: "can't be blank" }
 
-  belongs_to :days_to_ship_id
-  validates :days_to_ship_id, presence: true, numericality: { other_than: 1 , message: "can't be blank" }
+  belongs_to :condition
+  validates :condition_id, numericality: { other_than: 1 , message: "can't be blank" }
 
-  belongs_to :category_id
-  validates :category_id, presence: true, numericality: { other_than: 1 , message: "can't be blank" }
+  belongs_to :shipping_charge
+  validates :shipping_charge_id, numericality: { other_than: 1 , message: "can't be blank" }
 
-  belongs_to :condition_id
-  validates :category_id, presence: true, numericality: { other_than: 1 , message: "can't be blank" }
+  belongs_to :prefecture
+  validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank" }
 
-  belongs_to :shipping_charge_id
-  validates :category_id, presence: true, numericality: { other_than: 1 , message: "can't be blank" }
-
+  belongs_to :days_to_ship
+  validates :days_to_ship_id, numericality: { other_than: 1 , message: "can't be blank" }
 
 end
