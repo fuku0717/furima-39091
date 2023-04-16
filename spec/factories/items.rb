03b044684676b675475f066b163dd12106d58f6a
fuku0717 <zhenchengs314@gmail.com>
2023-04-16 15:37:48 +0900

    単体テストコード

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,16 +1,17 @@
 FactoryBot.define do
   factory :item do
-    item_name          {'商品名'}
-    explanation        {'商品の説明'}
-    category_id        {'2'}
-    condition_id       {'2'}
-    shipping_charge_id {'2'}
-    prefecture_id      {'2'}
-    days_to_ship_id    {'2'}
-    price              {'1234'}
-    association :user 
+    image              { Faker::Lorem.sentence }
+    item_name          { Faker::Commerce.product_name }
+    explanation        { Faker::Lorem.sentence }
+    category_id        { Faker::Number.between(from: 2, to: 11) }
+    condition_id       { Faker::Number.between(from: 2, to: 7) }
+    shipping_charge_id { Faker::Number.between(from: 2, to: 3) }
+    prefecture_id      { Faker::Number.between(from: 2, to: 48) }
+    days_to_ship_id    { Faker::Number.between(from: 2, to: 4) }
+    price              { Faker::Number.between(from: 300, to: 9_999_999) }
+    association :user
     after(:build) do |item|
-      item.image.attach(io: File.open('public/images/timage.png'), filename: 'timage.png')
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
     end
   end
 end
