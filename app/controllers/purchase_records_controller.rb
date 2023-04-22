3ff238a3dee55133368ba6130256a3ce780e8fcb
fuku0717 <zhenchengs314@gmail.com>
2023-04-22 23:25:14 +0900

    クレジットカード決済

--- app/controllers/purchase_records_controller.rb
+++ app/controllers/purchase_records_controller.rb
@@ -10,6 +10,7 @@ class PurchaseRecordsController < ApplicationController
      @item = Item.find(params[:item_id])
      @purchase_address = PurchaseAddress.new(purchase_address_params)
      if @purchase_address.valid?
+        pay_item
         @purchase_address.save
         redirect_to root_path
      else
@@ -20,7 +21,16 @@ class PurchaseRecordsController < ApplicationController
    private
 
    def purchase_address_params
-     params.require(:purchase_address).permit(:post_code, :prefecture_id, :municipalities, :address, :building_name, :telephone_number).merge(user_id: current_user.id, item_id: @item.id)
+     params.require(:purchase_address).permit(:post_code, :prefecture_id, :municipalities, :address, :building_name, :telephone_number).merge(user_id: current_user.id, item_id: @item.id, token: params[:token])
    end
    
+   def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+      Payjp::Charge.create(
+        amount: order_params[:price],  # 商品の値段
+        card: order_params[:token],    # カードトークン
+        currency: 'jpy'                 # 通貨の種類（日本円）
+      )
+   end
+
 end
