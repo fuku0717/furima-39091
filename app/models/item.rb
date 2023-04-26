4114b82c85d4bf98187817c00b8a21758950a69d
fuku0717 <zhenchengs314@gmail.com>
2023-04-26 22:55:28 +0900

    新規登録、商品出品機能のエラーメッセージの日本語化

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,8 +3,8 @@ class Item < ApplicationRecord
   validates :item_name, presence: true
   validates :explanation, presence: true
   validates :price, presence: true
-  validates :price, numericality: { only_integer: true, message: 'is abailable only half-width number' }
-  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: 'is out of setting range' }
+  validates :price, numericality: { only_integer: true, message: 'は半角数値のみ保存可能です' }
+  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: 'は¥300~¥9,999,999の間のみ保存可能です' }
 
   belongs_to :user
   has_one_attached :image
@@ -12,18 +12,18 @@ class Item < ApplicationRecord
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
-  validates :category_id, numericality: { other_than: 1 , message: "can't be blank" }
+  validates :category_id, numericality: { other_than: 1 , message: "を選択して下さい" }
 
   belongs_to :condition
-  validates :condition_id, numericality: { other_than: 1 , message: "can't be blank" }
+  validates :condition_id, numericality: { other_than: 1 , message: "を選択して下さい" }
 
   belongs_to :shipping_charge
-  validates :shipping_charge_id, numericality: { other_than: 1 , message: "can't be blank" }
+  validates :shipping_charge_id, numericality: { other_than: 1 , message: "を選択して下さい" }
 
   belongs_to :prefecture
-  validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank" }
+  validates :prefecture_id, numericality: { other_than: 1 , message: "を選択して下さい" }
 
   belongs_to :days_to_ship
-  validates :days_to_ship_id, numericality: { other_than: 1 , message: "can't be blank" }
+  validates :days_to_ship_id, numericality: { other_than: 1 , message: "を選択して下さい" }
 
 end
